fe3b168a0bf31ccd593c7cf2092e70d996650386
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-28 22:50:59 +0300

    Updated Readme.md, added solutions

---
+++ assessment1.py
@@ -0,0 +1,32 @@
+def is_balanced(expression):
+    l = []
+    condition = True
+    
+    if len(expression) % 2 != 0:
+        return False
+    
+    for c in expression:
+        if condition:
+            if c in '{[(':
+                l.append(c)
+            elif c in '}])':
+                if len(l) == 0:
+                    condition = False
+                else:
+                    last = l.pop()
+                    match c:
+                        case '}':
+                            condition = '{' == last
+                        case ']':
+                            condition = '[' == last
+                        case ')':
+                            condition = '(' == last
+            else:
+                condition = False
+    
+    return condition
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))  # Output: True
+print(is_balanced(expression2))  # Output: False 
